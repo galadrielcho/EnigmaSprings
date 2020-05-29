5e56ffcdf4e5be99e229c1eb3d6795ee99d8054d
loriencho <42721802+loriencho@users.noreply.github.com>
2020-05-29 13:34:52 -0400

    added NPC asnd player spritesheets, imported Softball font, started DialogueBox script

---
+++ Assets/DialogueBox.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DialogueBox : MonoBehaviour
+{
+
+    public string name;
+    public int size;
+    public List<string> dialogues;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
