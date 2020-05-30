135011f52b8c9ad146af1ca44f26c4b7d5dfcd69
loriencho <42721802+loriencho@users.noreply.github.com>
2020-05-29 22:27:17 -0400

    worked on dialogue system: added box asset, text; started game manager script; edited dialoguebox scripting

--- Assets/DialogueBox.cs
+++ Assets/DialogueBox.cs
@@ -1,18 +1,17 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class DialogueBox : MonoBehaviour
 {
 
-    public string name;
-    public int size;
-    public List<string> dialogues;
+    public string speaker;
+    public string[] dialogues = new string[5];
 
     // Start is called before the first frame update
     void Start()
     {
-        
     }
 
     // Update is called once per frame
