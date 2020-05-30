c0ec0ba6e35b151ba5bca5f5acca4c1a9e8f4f2d
galadrielcho <42721783+galadrielcho@users.noreply.github.com>
2020-05-30 13:18:48 -0400

    added wasd/arrows player sprite movement

---
+++ enigmasprings/Assets/DialogueBox.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class DialogueBox : MonoBehaviour
+{
+
+    public string speaker;
+    public string[] dialogues = new string[5];
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ enigmasprings/Assets/GameManagerScript.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class GameManagerScript : MonoBehaviour
+{
+    public static int day=1;
+    public TextMeshProUGUI field_dialogue; 
+    public TextMeshProUGUI field_speaker; 
+    public GameObject field_textbox;
+    public static TextMeshProUGUI dialogue; 
+    public static TextMeshProUGUI speaker; 
+    public static GameObject textbox;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        dialogue = field_dialogue;
+        speaker = field_speaker;
+        textbox = field_textbox;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
