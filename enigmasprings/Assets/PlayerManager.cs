fc86f5f15ac8e9e095e6d8b35532c931abca0b2c
galadrielcho <42721783+galadrielcho@users.noreply.github.com>
2020-05-30 23:27:24 -0400

    added player changing direction
    when moved, player sprite is changed to face right, left, top, or bottom

--- enigmasprings/Assets/PlayerManager.cs
+++ enigmasprings/Assets/PlayerManager.cs
@@ -4,20 +4,40 @@ using UnityEngine;
 
 public class PlayerManager : MonoBehaviour
 {
+    
     public float speed = 3f;
+    private SpriteRenderer sR;
+    public Sprite[] heroArray;
+
     void Start()
     {
+        sR = GetComponent<SpriteRenderer>();
+
     }
 
     void FixedUpdate()
     {
-        if (Input.GetKey(KeyCode.UpArrow)  ||  Input.GetKey(KeyCode.W))
+        if (Input.GetKey(KeyCode.UpArrow)  ||  Input.GetKey(KeyCode.W)) {
+            sR.flipX = false;
+            sR.sprite = heroArray[3];
             transform.Translate(Vector3.up * Time.deltaTime * speed);
-        else if (Input.GetKey(KeyCode.DownArrow)  ||  Input.GetKey(KeyCode.A))
+
+        }
+        else if (Input.GetKey(KeyCode.DownArrow)  ||  Input.GetKey(KeyCode.A)){
+            sR.flipX = false;
+            sR.sprite = heroArray[7];
             transform.Translate(Vector3.down * Time.deltaTime * speed);
-        else if (Input.GetKey(KeyCode.LeftArrow)  ||  Input.GetKey(KeyCode.S))
+
+        }
+        else if (Input.GetKey(KeyCode.LeftArrow)  ||  Input.GetKey(KeyCode.S)) {
+            sR.sprite = heroArray[0];
+            sR.flipX = false;
             transform.Translate(Vector3.left * Time.deltaTime * speed);
-        else if (Input.GetKey(KeyCode.RightArrow)  ||  Input.GetKey(KeyCode.D))
+        }
+        else if (Input.GetKey(KeyCode.RightArrow)  ||  Input.GetKey(KeyCode.D)) {
+            sR.sprite = heroArray[1];
+            sR.flipX = true;
             transform.Translate(Vector3.right * Time.deltaTime * speed);
+        }
     }
 }
