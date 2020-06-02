584dab6fa04156577f3fb1e5cf734ead8b6c547f
loriencho <42721802+loriencho@users.noreply.github.com>
2020-06-02 19:00:33 -0400

    changed the way camera follows player, edited box collider around player, and prevented player movement into buildings

--- Assets/PlayerManager.cs
+++ Assets/PlayerManager.cs
@@ -11,6 +11,8 @@ public class PlayerManager : MonoBehaviour
     public Camera mainCamera;
     public Animator animator;
 
+    private bool isTouching = false;
+
     void Start()
     {
         sR = GetComponent<SpriteRenderer>();
@@ -19,37 +21,36 @@ public class PlayerManager : MonoBehaviour
 
     void FixedUpdate()
     {
+
+        
         if (Input.GetKey(KeyCode.UpArrow)  ||  Input.GetKey(KeyCode.W)) {
-            sR.flipX = false;
-            animator.SetInteger("direction", 1);
-            transform.Translate(Vector3.up * Time.deltaTime * speed);
-            mainCamera.transform.Translate(Vector3.up * Time.deltaTime * speed);
+        sR.flipX = false;
+        animator.SetInteger("direction", 1);
+        transform.Translate(Vector3.up * Time.deltaTime * speed);
 
-
-        }
+    }       
         else if (Input.GetKey(KeyCode.DownArrow)  ||  Input.GetKey(KeyCode.S)){
             sR.flipX = false;
             animator.SetInteger("direction", 2);
             transform.Translate(Vector3.down * Time.deltaTime * speed);
-            mainCamera.transform.Translate(Vector3.down * Time.deltaTime * speed);
 
         }
         else if (Input.GetKey(KeyCode.LeftArrow)  ||  Input.GetKey(KeyCode.A)) {
             animator.SetInteger("direction", 3);
             sR.flipX = false;
-            transform.Translate(Vector3.left * Time.deltaTime * speed);
-            mainCamera.transform.Translate(Vector3.left * Time.deltaTime * speed);
+            transform.Translate(Vector3.left * Time.deltaTime * speed); 
+            }
 
-        }
         else if (Input.GetKey(KeyCode.RightArrow)  ||  Input.GetKey(KeyCode.D)) {
             animator.SetInteger("direction", 3);
             sR.flipX = true;
-            mainCamera.transform.Translate(Vector3.right * Time.deltaTime * speed);
-            transform.Translate(Vector3.right * Time.deltaTime * speed);
+            transform.Translate(Vector3.right * Time.deltaTime * speed); 
+
         }
         else {
             animator.SetInteger("direction", 0);
             sR.sprite = heroArray[animator.GetInteger("direction")];
         }
+        
     }
 }
