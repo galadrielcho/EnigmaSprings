3fbef2adeb0fbfaf223c472c90e949558d7785f0
Alemar23 <60222598+Alemar23@users.noreply.github.com>
2020-06-03 14:10:50 -0400

    Add files via upload

---
+++ Assets/ProximityChat.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using JetBrains.Annotations;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ProximityChat : MonoBehaviour
+{
+    public Transform player;
+    public GameObject uiCanv;
+
+
+
+    void Update()
+    {
+        GameObject.FindGameObjectWithTag("Player");
+
+        float dist = Vector3.Distance(GameObject.FindGameObjectWithTag("Player").transform.position, transform.position);
+
+        if (dist < 2)
+        {
+
+            uiCanv.SetActive(true);
+
+        }
+        else
+        {
+            uiCanv.SetActive(false);
+        }
+
+
+
+    }
+}
