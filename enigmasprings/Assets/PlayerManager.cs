403905c145366c0ccfd2c0e45c4cf97a799d4862
galadrielcho <42721783+galadrielcho@users.noreply.github.com>
2020-06-09 22:31:32 -0400

    stopped walkcycle brief continuation after player stopps moving, but discovered slight lag when changing directions

--- enigmasprings/Assets/PlayerManager.cs
+++ enigmasprings/Assets/PlayerManager.cs
@@ -13,6 +13,7 @@ public class PlayerManager : MonoBehaviour
     void Start()
     {
         sR = GetComponent<SpriteRenderer>();
+   
 
     }
 
@@ -21,34 +22,43 @@ public class PlayerManager : MonoBehaviour
     {
 
         if (!GameManagerScript.speaking) {
+            animator.speed = 1;
+            // Player moves and looks up if up arrow or 'w' key is hit
             if (Input.GetKey(KeyCode.UpArrow)  ||  Input.GetKey(KeyCode.W)) {
-                sR.flipX = false;
-                animator.SetInteger("direction", 1);
-                transform.Translate(Vector3.up * Time.deltaTime * speed);
-
-        }       
-            else if (Input.GetKey(KeyCode.DownArrow)  ||  Input.GetKey(KeyCode.S)){
-                sR.flipX = false;
-                animator.SetInteger("direction", 2);
-                transform.Translate(Vector3.down * Time.deltaTime * speed);
-
-            }
-            else if (Input.GetKey(KeyCode.LeftArrow)  ||  Input.GetKey(KeyCode.A)) {
-                animator.SetInteger("direction", 3);
-                sR.flipX = false;
-                transform.Translate(Vector3.left * Time.deltaTime * speed); 
+                    sR.flipX = false;
+                    animator.SetInteger("direction", 1);
+                    transform.Translate(Vector3.up * Time.deltaTime * speed);
                 }
-
+            // Player moves and looks down if down arrow or 's' key is hit   
+            else if (Input.GetKey(KeyCode.DownArrow)  ||  Input.GetKey(KeyCode.S)){
+                    sR.flipX = false;
+                    animator.SetInteger("direction", 2);
+                    transform.Translate(Vector3.down * Time.deltaTime * speed);
+                }
+            // Player moves and looks left if left arrow or 'a' key is hit   
+            else if (Input.GetKey(KeyCode.LeftArrow)  ||  Input.GetKey(KeyCode.A)) {
+                    sR.flipX = false;
+                    animator.SetInteger("direction", 3);
+                    transform.Translate(Vector3.left * Time.deltaTime * speed); 
+                    }
+            // Player moves and looks right if right arrow or 'd' key is hit   
             else if (Input.GetKey(KeyCode.RightArrow)  ||  Input.GetKey(KeyCode.D)) {
-                animator.SetInteger("direction", 3);
-                sR.flipX = true;
-                transform.Translate(Vector3.right * Time.deltaTime * speed); 
+                    animator.SetInteger("direction", 3);
+                    sR.flipX = true;
+                    transform.Translate(Vector3.right * Time.deltaTime * speed); 
 
-            }
+                }
             else {
-                animator.SetInteger("direction", 0);
-                sR.sprite = heroArray[animator.GetInteger("direction")];
-            }
+                    animator.SetInteger("direction", 0);
+                    int direction = animator.GetInteger("direction");
+                    animator.speed = 0;
+                    sR.sprite = heroArray[direction];
+
+                }
+            
+
+        } else {
+            animator.speed = 0;
         }
         
     }
