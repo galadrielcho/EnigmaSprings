c5a39e7241765eed39716898ebb2b81edab4b5d3
loriencho <42721802+loriencho@users.noreply.github.com>
2020-07-07 00:21:49 -0400

    edited PlayerManager to work with touch input on movement controller

--- Assets/Scripts/PlayerManager.cs
+++ Assets/Scripts/PlayerManager.cs
@@ -10,6 +10,9 @@ public class PlayerManager : MonoBehaviour
     public Sprite[] heroArray;
     public Camera mainCamera;
     public Animator animator;
+    private Vector3 touchPosWorld;
+    private GameObject touchedObject;
+
     void Start()
     {
         sR = GetComponent<SpriteRenderer>();
@@ -21,49 +24,53 @@ public class PlayerManager : MonoBehaviour
     void FixedUpdate()
     {
 
-        if (!GameManagerScript.speaking) {
+
+        if (!GameManagerScript.speaking && Input.touchCount > 0 ) {
             animator.speed = 1;
-            // F speeds up player
-            if (Input.GetKey(KeyCode.F)) {
-                    speed = 10f;
-                }
-            else {
-                speed = 3f;
-            }
-            // Player moves and looks up if up arrow or 'w' key is hit
 
-            if (Input.GetKey(KeyCode.UpArrow)  ||  Input.GetKey(KeyCode.W)) {
-                    sR.flipX = false;
-                    animator.SetInteger("direction", 1);
-                    transform.Translate(Vector3.up * Time.deltaTime * speed);
-                }
-            // Player moves and looks down if down arrow or 's' key is hit   
-            else if (Input.GetKey(KeyCode.DownArrow)  ||  Input.GetKey(KeyCode.S)){
-                    sR.flipX = false;
-                    animator.SetInteger("direction", 2);
-                    transform.Translate(Vector3.down * Time.deltaTime * speed);
-                }
-            // Player moves and looks left if left arrow or 'a' key is hit   
-            else if (Input.GetKey(KeyCode.LeftArrow)  ||  Input.GetKey(KeyCode.A)) {
-                    sR.flipX = false;
-                    animator.SetInteger("direction", 3);
-                    transform.Translate(Vector3.left * Time.deltaTime * speed); 
+            // Fire raycast at touch position
+            touchPosWorld = Camera.main.ScreenToWorldPoint(Input.GetTouch(0).position);
+            Vector2 touchPosWorld2D= new Vector2(touchPosWorld.x, touchPosWorld.y);
+            RaycastHit2D hitInfo = Physics2D.Raycast(touchPosWorld2D, Camera.main.transform.forward);
+
+
+            // detect if raycast hit a movement control button collider
+            if (hitInfo.collider != null){    
+                touchedObject = hitInfo.transform.gameObject;
+
+                // Player moves and looks up if up arrow or 'w' key is hit
+                if (touchedObject.name == "up"  ||  Input.GetKey(KeyCode.W)) {
+                        sR.flipX = false;
+                        animator.SetInteger("direction", 1);
+                        transform.Translate(Vector3.up * Time.deltaTime * speed);
                     }
-            // Player moves and looks right if right arrow or 'd' key is hit   
-            else if (Input.GetKey(KeyCode.RightArrow)  ||  Input.GetKey(KeyCode.D)) {
-                    animator.SetInteger("direction", 3);
-                    sR.flipX = true;
-                    transform.Translate(Vector3.right * Time.deltaTime * speed); 
+                // Player moves and looks down if down arrow or 's' key is hit   
+                else if (touchedObject.name == "down"  ||  Input.GetKey(KeyCode.S)){
+                        sR.flipX = false;
+                        animator.SetInteger("direction", 2);
+                        transform.Translate(Vector3.down * Time.deltaTime * speed);
+                    }
+                // Player moves and looks left if left arrow or 'a' key is hit   
+                else if (touchedObject.name == "left"  ||  Input.GetKey(KeyCode.A)) {
+                        sR.flipX = false;
+                        animator.SetInteger("direction", 3);
+                        transform.Translate(Vector3.left * Time.deltaTime * speed); 
+                        }
+                // Player moves and looks right if right arrow or 'd' key is hit   
+                else if (touchedObject.name == "right"  ||  Input.GetKey(KeyCode.D)) {
+                        animator.SetInteger("direction", 3);
+                        sR.flipX = true;
+                        transform.Translate(Vector3.right * Time.deltaTime * speed); 
 
-                }
-            else {
-                    animator.SetInteger("direction", 0);
-                    int direction = animator.GetInteger("direction");
-                    animator.speed = 0;
-                    sR.sprite = heroArray[direction];
+                    }
+                else {
+                        animator.SetInteger("direction", 0);
+                        int direction = animator.GetInteger("direction");
+                        animator.speed = 0;
+                        sR.sprite = heroArray[direction];
 
-                }
-            
+                    }
+            }
 
         } else {
             animator.speed = 0;
